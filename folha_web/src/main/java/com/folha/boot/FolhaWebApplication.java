3c4736c3a6759f0124f7eee8ff7224aebfe4c9e7
andreicomcristo <76257108+andreicomcristo@users.noreply.github.com>
2021-01-31 15:10:14 -0300

    Add files via upload

---
+++ folha_web/src/main/java/com/folha/boot/FolhaWebApplication.java
@@ -0,0 +1,17 @@
+package com.folha.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolhaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolhaWebApplication.class, args);
+		
+		//ffffsó um comentário*/
+		//só um comentário*/  
+		
+	}
+
+}
