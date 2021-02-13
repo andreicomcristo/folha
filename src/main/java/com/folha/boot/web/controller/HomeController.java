3de12a4b5d33a3da3c52235950f35b59d34a4db2
andre <andre@andre-IPMH110P>
2021-02-13 07:32:32 -0300

    recuperar usuario logado

--- src/main/java/com/folha/boot/web/controller/HomeController.java
+++ src/main/java/com/folha/boot/web/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.folha.boot.web.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller  
+@Controller
 public class HomeController {
 
 	@GetMapping("/")
 	public String home() {
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
 		return "/home";
 	}
 }
