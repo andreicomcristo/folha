73d50471835f8062a72c7ebbb9b6111e625f033e
andreicomcristo <76257108+andreicomcristo@users.noreply.github.com>
2021-03-01 12:06:22 -0300

    lista de unidades no login

--- src/main/java/com/folha/boot/web/controller/LoginController.java
+++ src/main/java/com/folha/boot/web/controller/LoginController.java
@@ -1,15 +1,24 @@
 package com.folha.boot.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.folha.boot.domain.Unidades;
+import com.folha.boot.service.UnidadesService;
 
 @Controller
 public class LoginController {
  
+	@Autowired
+	UnidadesService unidadesService;
+	
 	@GetMapping
 	@RequestMapping("/login")  
-	public String login() {
+	public String login(Unidades unidades , ModelMap model) {
+		model.addAttribute("unidades", unidadesService.buscarPorNome(""));
 		return "login";
 	}
 }
