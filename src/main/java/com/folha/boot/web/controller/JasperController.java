c119dd64dd2ddecfef4ede235ff68e045439acb9
andre <andre@andre-IPMH110P>
2021-05-14 20:08:32 -0300

    relatorios Jasper-Ainda não funciona

---
+++ src/main/java/com/folha/boot/web/controller/JasperController.java
@@ -0,0 +1,42 @@
+package com.folha.boot.web.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import com.folha.boot.service.relatorios.JasperService;
+
+@Controller
+public class JasperController {
+
+	@Autowired
+	private JasperService service;
+	
+	@GetMapping("/reports")
+	public String abrir() {
+		
+		return "reports";
+	}
+	
+	@GetMapping("/relatorio/pdf/jr1")
+	public void exibirRelatorio(@RequestParam("code") String code, 
+								@RequestParam("acao") String acao,
+								HttpServletResponse response) throws IOException {
+		byte[] bytes = service.exportarPDF(code);
+		response.setContentType(org.springframework.http.MediaType.APPLICATION_PDF_VALUE);
+		
+		if(acao.equals("v")){
+			response.setHeader("Content-disposition", "inline; filename=relatorio-"+code+".pdf");		
+		}else {
+			response.setHeader("Content-disposition", "attachment; filename=relatorio-"+code+".pdf");
+		}
+		response.getOutputStream().write(bytes);
+	}
+		
+}
