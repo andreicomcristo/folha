4ed559f46637cf9c348cbf3ba71d729b7526d71a
andre <andre@andre-IPMH110P>
2021-05-20 18:59:18 -0300

    renomeação da classe de conexão jasper

--- src/main/java/com/folha/boot/config/JasperConnection.java
+++ src/main/java/com/folha/boot/config/JasperConnection.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ConfiguracaoGenerica {
+public class JasperConnection {
 	
 	@Bean
 	public Connection connection (DataSource dataSource) throws SQLException {
