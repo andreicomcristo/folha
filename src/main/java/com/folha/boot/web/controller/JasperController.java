02629c3a61319b4eaaa7bbee584ed9f76773058a
andreicomcristo <76257108+andreicomcristo@users.noreply.github.com>
2021-05-21 16:22:17 -0300

    primeiro grafico

--- src/main/java/com/folha/boot/web/controller/JasperController.java
+++ src/main/java/com/folha/boot/web/controller/JasperController.java
@@ -2,12 +2,14 @@ package com.folha.boot.web.controller;
 
 import java.io.IOException;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -44,20 +46,199 @@ public class JasperController {
 	}	
 	
 
-	@GetMapping("/abrirRelatoriosFolha")
-	public String abrirRelatoriosFolha() {		
-		return "/reports/reportsFolha";
+
+	
+	
+	//variacaoCustoPorUnidade
+	@GetMapping("/abrirRelatoriosFolha/variacaoCustoPorUnidade")
+	public String abrirRelatoriosFolhaVariacaoCustoPorUnidade() {		
+		return "/reports/variacaoCustoPorUnidade";
 	}
 
-	@GetMapping("/relatoriosFolha")
-	public void exibirRelatoriosFolha(@RequestParam("ano") String ano, HttpServletResponse response) throws IOException {
-		
+	@GetMapping("/relatoriosFolha/variacaoCustoPorUnidade")
+	public void exibirRelatoriosFolhaVariacaoCustoPorUnidade(@RequestParam("ano") String ano, HttpServletResponse response) throws IOException {
 		if(ano.length()==4) {ano = ano+"%";}
 		service.addParametros("ANO_I", ano);		
-		service.setCaminho("/jasper/folha/VariacaoCustoPorUnidade.jasper");
+		service.setCaminho("/jasper/folha/variacaoCustoPorUnidade.jasper");
 		byte[] bytes = service.gerarRelatorio(); 
 		response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+		//Faz o download
+		response.setHeader("Content-disposition", "attachment; filename=dados.pdf");
 		response.getOutputStream().write(bytes);
 	}	
+	
 
+	
+	//variacaoCustoPorFonte
+	@GetMapping("/abrirRelatoriosFolha/variacaoCustoPorFonte")
+	public String abrirRelatoriosFolhavariacaoCustoPorFonte() {		
+		return "/reports/variacaoCustoPorFonte";
+	}
+
+	@GetMapping("/relatoriosFolha/variacaoCustoPorFonte")
+	public void exibirRelatoriosFolhavariacaoCustoPorFonte(@RequestParam("ano") String ano, HttpServletResponse response) throws IOException {
+		if(ano.length()==4) {ano = ano+"%";}
+		service.addParametros("ANO_I", ano);		
+		service.setCaminho("/jasper/folha/variacaoCustoPorFonte.jasper");
+		byte[] bytes = service.gerarRelatorio(); 
+		response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+		//Faz o download
+		response.setHeader("Content-disposition", "attachment; filename=dados.pdf");
+		response.getOutputStream().write(bytes);
+	}	
+	
+	
+	
+	//variacaoCustoPorMes
+	@GetMapping("/abrirRelatoriosFolha/variacaoCustoPorMes")
+	public String abrirRelatoriosFolhavariacaoCustoPorMes() {		
+		return "/reports/variacaoCustoPorMes";
+	}
+
+	@GetMapping("/relatoriosFolha/variacaoCustoPorMes")
+	public void exibirRelatoriosFolhavariacaoCustoPorMes(@RequestParam("ano") String ano, HttpServletResponse response) throws IOException {
+		if(ano.length()==4) {ano = ano+"%";}
+		service.addParametros("ANO_I", ano);		
+		service.setCaminho("/jasper/folha/variacaoCustoPorMes.jasper");
+		byte[] bytes = service.gerarRelatorio(); 
+		response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+		//Faz o download
+		response.setHeader("Content-disposition", "attachment; filename=dados.pdf");
+		response.getOutputStream().write(bytes);
+	}	
+		
+	
+	
+	
+	//variacaoCustoPorUnidadeArea
+	@GetMapping("/abrirRelatoriosFolha/variacaoCustoPorUnidadeArea")
+	public String abrirRelatoriosFolhavariacaoCustoPorUnidadeArea() {		
+		return "/reports/variacaoCustoPorUnidadeArea";
+	}
+
+	@GetMapping("/relatoriosFolha/variacaoCustoPorUnidadeArea")
+	public void exibirRelatoriosFolhavariacaoCustoPorUnidadeArea(@RequestParam("ano") String ano, HttpServletResponse response) throws IOException {
+		if(ano.length()==4) {ano = ano+"%";}
+		service.addParametros("ANO_I", ano);		
+		service.setCaminho("/jasper/folha/variacaoCustoPorUnidadeArea.jasper");
+		byte[] bytes = service.gerarRelatorio(); 
+		response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+		//Faz o download
+		response.setHeader("Content-disposition", "attachment; filename=dados.pdf");
+		response.getOutputStream().write(bytes);
+	}	
+		
+	
+	
+	
+	//variacaoCustoPorUnidadeFolha
+	@GetMapping("/abrirRelatoriosFolha/variacaoCustoPorUnidadeFolha")
+	public String abrirRelatoriosFolhavariacaoCustoPorUnidadeFolha() {		
+		return "/reports/variacaoCustoPorUnidadeFolha";
+	}
+
+	@GetMapping("/relatoriosFolha/variacaoCustoPorUnidadeFolha")
+	public void exibirRelatoriosFolhavariacaoCustoPorUnidadeFolha(@RequestParam("ano") String ano, HttpServletResponse response) throws IOException {
+		if(ano.length()==4) {ano = ano+"%";}
+		service.addParametros("ANO_I", ano);		
+		service.setCaminho("/jasper/folha/variacaoCustoPorUnidadeFolha.jasper");
+		byte[] bytes = service.gerarRelatorio(); 
+		response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+		//Faz o download
+		response.setHeader("Content-disposition", "attachment; filename=dados.pdf");
+		response.getOutputStream().write(bytes);
+	}		
+	
+	
+	//variacaoCustoPorUnidadeNivel
+	@GetMapping("/abrirRelatoriosFolha/variacaoCustoPorUnidadeNivel")
+	public String abrirRelatoriosFolhavariacaoCustoPorUnidadeNivel() {		
+		return "/reports/variacaoCustoPorUnidadeNivel";
+	}
+
+	@GetMapping("/relatoriosFolha/variacaoCustoPorUnidadeNivel")
+	public void exibirRelatoriosFolhavariacaoCustoPorUnidadeNivel(@RequestParam("ano") String ano, HttpServletResponse response) throws IOException {
+		if(ano.length()==4) {ano = ano+"%";}
+		service.addParametros("ANO_I", ano);		
+		service.setCaminho("/jasper/folha/variacaoCustoPorUnidadeNivel.jasper");
+		byte[] bytes = service.gerarRelatorio(); 
+		response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+		//Faz o download
+		response.setHeader("Content-disposition", "attachment; filename=dados.pdf");
+		response.getOutputStream().write(bytes);
+	}	
+	
+	
+	
+	
+	//variacaoCustoPorUnidadeNivelCargo
+	@GetMapping("/abrirRelatoriosFolha/variacaoCustoPorUnidadeNivelCargo")
+	public String abrirRelatoriosFolhavariacaoCustoPorUnidadeNivelCargo() {		
+		return "/reports/variacaoCustoPorUnidadeNivelCargo";
+	}
+
+	@GetMapping("/relatoriosFolha/variacaoCustoPorUnidadeNivelCargo")
+	public void exibirRelatoriosFolhavariacaoCustoPorUnidadeNivelCargo(@RequestParam("ano") String ano, HttpServletResponse response) throws IOException {
+		if(ano.length()==4) {ano = ano+"%";}
+		service.addParametros("ANO_I", ano);		
+		service.setCaminho("/jasper/folha/variacaoCustoPorUnidadeNivelCargo.jasper");
+		byte[] bytes = service.gerarRelatorio(); 
+		response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+		//Faz o download
+		response.setHeader("Content-disposition", "attachment; filename=dados.pdf");
+		response.getOutputStream().write(bytes);
+	}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//GRAFICOS
+	//variacaoCustoPorMes_grafico
+	@GetMapping("/abrirRelatoriosFolha/variacaoCustoPorMes_grafico")
+	public String abrirRelatoriosFolhavariacaoCustoPorMes_grafico() {		
+		return "/reports/variacaoCustoPorMes_grafico";
+	}
+
+	@GetMapping("/relatoriosFolha/variacaoCustoPorMes_grafico")
+	public void exibirRelatoriosFolhavariacaoCustoPorMes_grafico(@RequestParam("ano") String ano, HttpServletResponse response) throws IOException {
+		if(ano.length()==4) {ano = ano+"%";}
+		service.addParametros("ANO_I", ano);		
+		service.setCaminho("/jasper/folha/variacaoCustoPorMes_grafico.jasper");
+		byte[] bytes = service.gerarRelatorio(); 
+		response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+		//Faz o download
+		response.setHeader("Content-disposition", "attachment; filename=dados.pdf");
+		response.getOutputStream().write(bytes);
+	}	
+	
+	
+	
+	
+	
+	
+	
+	
+	@Autowired
+	HttpServletRequest request;
+	@ModelAttribute("nomeOperadorLogado")
+	public String operadorLogado() {
+		return request.getSession().getAttribute("operador").toString();
+	}
+	@ModelAttribute("nomeUnidadeLogada")
+	public String unidadeLogada() {
+		return request.getSession().getAttribute("unidade").toString();
+	}	
+	
+	
+	
+	
+	
 }
