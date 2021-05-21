78c07cde72c29f8455c0c24d97626fccea7199fe
andreicomcristo <76257108+andreicomcristo@users.noreply.github.com>
2021-05-21 10:40:28 -0300

    relatorios

--- src/main/java/com/folha/boot/web/controller/JasperController.java
+++ src/main/java/com/folha/boot/web/controller/JasperController.java
@@ -8,11 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 import com.folha.boot.service.relatorios.JasperService;
-
+@RequestMapping("/jasper")
 @Controller
 public class JasperController {
 
@@ -36,5 +37,23 @@ public class JasperController {
 		byte[] bytes = service.gerarRelatorio(); 
 		response.setContentType(MediaType.APPLICATION_PDF_VALUE);
 		response.getOutputStream().write(bytes);
-	}		
+	}	
+	
+
+	@GetMapping("/abrirRelatoriosFolha")
+	public String abrirRelatoriosFolha() {		
+		return "/reports/reportsFolha";
+	}
+
+	@GetMapping("/relatoriosFolha")
+	public void exibirRelatoriosFolha(@RequestParam("ano") String ano, HttpServletResponse response) throws IOException {
+		
+		if(ano.length()==4) {ano = ano+"%";}
+		service.addParametros("ANO_I", ano);		
+		service.setCaminho("/jasper/folha/VariacaoCustoPorUnidade.jasper");
+		byte[] bytes = service.gerarRelatorio(); 
+		response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+		response.getOutputStream().write(bytes);
+	}	
+
 }
